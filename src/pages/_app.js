885736e7cdd35fc0b97f8b007c27de9dc9900252
Gchism94 <gchism@arizona.edu>
2023-04-11 12:58:30 -0700

    Add shrink-to-fit=no

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main
